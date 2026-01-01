dd5f95483286f7dbfa9e69a288d98d2dfbdfe156
ernakha <ernakhasanah78@gmail.com>
2026-01-01 21:14:33 +0700

    crud golongan

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -40,6 +40,7 @@
                  ========================================== -->
             <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-6">
                 @yield('content')
+                @stack('scripts')
 
             </main>
 
