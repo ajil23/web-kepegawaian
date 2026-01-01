becb50dea8322e88e5447f3faf98859a1e33fe8d
ernakha <ernakhasanah78@gmail.com>
2026-01-01 14:05:02 +0700

    add migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
+            $table->string('nip');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('pegawai');
+            $table->string('role')->default('pegawai')->values(['admin', 'pegawai','kph']);
+            $table->string('status_akun')->default('nonaktif')->values(['aktif','nonaktif']);
+            $table->string('catatan_verifikasi');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2026_01_01_065731_create_catatan_kegiatan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('catatan_kegiatan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('pegawai_id');
+            $table->foreign('pegawai_id')->references('id')->on('pegawai')->onDelete('cascade')->onUpdate('cascade');
+            $table->month('periode_bulan');
+            $table->year('periode_tahun');
+            $table->text('judul');
+            $table->text('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('catatan_kegiatan');
+    }
+};
